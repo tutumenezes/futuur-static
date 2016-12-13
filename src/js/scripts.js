e60681274df6526a2c5c6f0f521cda8d4d31192c
Arthur Moreira Menezes <tutumenezes@gmail.com>
2016-12-13 12:15:59 -0200

    wowjs and animatecss implemented

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -14,10 +14,12 @@
 
 
   $(".sendgrid-subscription-widget").on("success", function (e) {
+    // Do something w/ success and activate success lightbox
     $(this).find(".response").addClass("success").html("<img src=\"colon/d/face.png\" alt=\"YAY!\"> " + e.detail);
   });
 
   $(".sendgrid-subscription-widget").on("error", function (e) {
+    // Do something w/ error and activate error lightbox
     $(".lightbox-").addClass("active");
   });
 
